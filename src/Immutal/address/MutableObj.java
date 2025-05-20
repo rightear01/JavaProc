7605b8cf6b2ef7138c4629a8eb57ab19daace9f3
Cha jeong hawn <rightear01@gmail.com>
2025-05-20 23:59:26 +0900

    Update MutableObj.java

--- src/Immutal/address/MutableObj.java
+++ src/Immutal/address/MutableObj.java
@@ -1,4 +1,21 @@
 package Immutal.address;
 
 public class MutableObj {
+        private int value;
+
+        MutableObj(int value) {
+            this.value = value;
+        }
+
+        public void add(int addValue){
+            value = value + addValue;
+        }
+
+        public int getValue(){
+            return value;
+        }
+
+        public void setValue( int value){
+            this.value = value;
+        }
 }
